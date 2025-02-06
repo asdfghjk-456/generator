6c01d8504ce1c80cdd48d3f3525dfa033e14e54b
asdfghjk-456 <rustikdon95@gmail.com>
2025-02-06 11:37:36 +0300

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,11 @@
+def prime_generator(n: int):
+    for num in range(2, n):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+for prime in prime_generator(15):
+    print(prime)
